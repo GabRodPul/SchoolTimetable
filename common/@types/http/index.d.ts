5b974c7ef47801d7cf07d2e34ca22d1fef72f893
Gabriel Rodríguez <gabrodpul@gmail.com>
2024-11-05 20:04:56 +0000

    .env xddd

--- common/@types/http/index.d.ts
+++ common/@types/http/index.d.ts
@@ -1,6 +1,7 @@
 declare namespace e {
-    type ResponseData 
-        = { code: 400, errors: string[] }
+    type ResponseData<T> 
+        = T
+        | { code: 400, errors: string[] }
         | { code: number, message: string  }
 }
 
