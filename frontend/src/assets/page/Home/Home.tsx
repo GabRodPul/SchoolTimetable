d51f50612f4e1b0fcc8564caf4518aa568c8d4ee
Smosherty <Danielmatiassmo@gmail.com>
2024-11-06 20:00:59 +0000

    Base del menu desplegable

--- frontend/src/assets/page/Home/Home.tsx
+++ frontend/src/assets/page/Home/Home.tsx
@@ -1,7 +1,7 @@
 import Header from '../../componets/CommonComponets/Header/Header';
 import Footer from '../../componets/ComponentsHome/Footer/Footer';
-import FrontImg from '../../componets/ComponentsHome/FrontImg/FrontIng'
-import HomeCard from '../../componets/ComponentsHome/HomeCards/HomeCard'
+import FrontImg from '../../componets/ComponentsHome/FrontImg/FrontIng';
+import HomeCard from '../../componets/ComponentsHome/HomeCards/HomeCard';
 
 function Home() {
     return (
