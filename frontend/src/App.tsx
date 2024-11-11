f9c675910953ae6c0bd674312a6b7f0445a1a2e2
Smosherty <Danielmatiassmo@gmail.com>
2024-11-11 20:43:06 +0000

    Peruanos en aceite

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,6 +5,7 @@ import Course from './assets/page/CoursePage/Course'
 import CourseFormPage from './assets/page/CourseFormPage/CourseFormPage'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import GroupPage from './assets/page/GroupPage/GroupPage'
+import NoticesPage from './assets/page/NoticesPage/NoticesPage'
 
 function App() {
 
@@ -15,6 +16,7 @@ function App() {
         <Routes>
           <Route path='/' element={<Home />} />
           <Route path='/Home' element={<Home />} />
+          <Route path='/NoticesPage' element={<NoticesPage />} />
           <Route path='/Course' element={<Course />} />
           <Route path='/CourseFormPage' element={<CourseFormPage />} />
           <Route path='/GroupPage' element={<GroupPage />} />
--- frontend/src/assets/componets/ComponentsHome/HomeCards/HomeCard.tsx
+++ frontend/src/assets/componets/ComponentsHome/HomeCards/HomeCard.tsx
@@ -9,19 +9,19 @@ function HomeCard() {
                 <FontAwesomeIcon icon={faClock} />
                 <h5>Página de Horarios</h5>
                 <p>Vea sus horarios</p>
-                <button>Acceder</button>
+                <a href="http://"><button>Acceder</button></a>
             </div>
             <div className="card">
                 <FontAwesomeIcon icon={faBell} />
-                <h5>Página de Horarios</h5>
-                <p>Vea sus horarios</p>
-                <button>Acceder</button>
+                <h5>Página de Avisos</h5>
+                <p>Vea sus avisos</p>
+                <a href="/NoticesPage"><button>Acceder</button></a>
             </div>
             <div className="card">
                 <FontAwesomeIcon icon={faFilePen} />
-                <h5>Página de Horarios</h5>
-                <p>Vea sus horarios</p>
-                <button>Acceder</button>
+                <h5>Página de Tramites</h5>
+                <p>Vea sus tramites</p>
+                <a href="http://"><button>Acceder</button></a>
             </div>
         </div>
     );
