e7fd96cea8db2b6ff7240751b7dbb029c94c5219
Smosherty <Danielmatiassmo@gmail.com>
2024-11-11 19:03:55 +0000

    commit para que que deje hacer pull

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,33 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+// import { fileURLToPath } from 'url'
+import path from 'path'
+
+const __dirname = path.dirname(__filename);
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, path.resolve(__dirname, './../'), "");
+  return {
+    plugins: [react()],
+    resolve: {
+      // alias: [ { find: '#common', replacement: fileURLToPath(new URL('../common', import.meta.url)) } ]
+      alias: { '#common': path.resolve(__dirname, './../common') }
+    },
+    // envDir: "../",
+    define: {
+      _APP_ENV_: env.APP_ENV
+    },
+    server: {
+      proxy: {
+        "/api": {
+          target: "https://localhost:8080",
+          changeOrigin: true,
+          secure: false,
+          rewrite: (path) => path.replace(/^\/api/, ""),
+        },
+      },
+    }
+    // build: { commonjsOptions: { esmExternals: true, } }
+  }
 })
