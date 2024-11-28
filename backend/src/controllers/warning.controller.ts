28abdbda4b664bca7d5b11fbfc450b8e537d4edf
DaniYezid <daniyezid@gmail.com>
2024-11-28 16:08:53 +0000

    Finishing things

--- backend/src/controllers/warning.controller.ts
+++ backend/src/controllers/warning.controller.ts
@@ -24,16 +24,42 @@ const WarningController = Object.freeze({
     },
 
     findOne: async (req: Request, res: Response) => {
-        res.send(resMsg(500, "Couldn't find one"));
+        try {
+            const data = await Warning.findByPk(req.params.id);
+            res.send(data);
+        } catch (err: any) {
+            res.send(computeError( err ));
+        }   
     },
 
     update: async (req: Request, res: Response) => {
-        res.send(resMsg(500, "Couldn't update"));
+        const id = req.params.id;
+        try {
+            const data = await Warning.findByPk(id);
+            if (!data)
+                throw new Error(`Warning with id ${id} not found`);
+
+            req.body.updatedAt = Date.now();
+            await data.update(req.body);
+            await data.save();
+        } catch (err: any) {
+            res.send(computeError(err, `Some error occurred while updating Warning with id ${id}`));
+        }
     },
 
     delete: async (req: Request, res: Response) => {
-        res.send(resMsg(500, "Couldn't delete"));
+        const id = req.params.id;
+        try {
+            const data = await Warning.destroy({ where: { id } });
+            if (!data)
+                throw new Error(`Warning with id ${id} not found`);
+
+            res.send(resMsg(204, "Succesfully deleted!"));
+        } catch(err: any) {
+            res.send(computeError(err, `Some error occurred while deleting Warning with id ${id}`));
+        }
     },
+    
 });
 
 export { WarningController };
