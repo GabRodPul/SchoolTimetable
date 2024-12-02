b53bb2d4fe3250fc7c2bba526528b1cd49c1b3bc
DaniYezid <daniyezid@gmail.com>
2024-12-02 15:42:46 +0000

    working on sidemenu.tsx

--- frontend/src/assets/componets/CommonComps/SideMenu/SideMenu.tsx
+++ frontend/src/assets/componets/CommonComps/SideMenu/SideMenu.tsx
@@ -1,20 +1,24 @@
 import './SideMenuStyles.css'
-
+import { RiCalendarScheduleLine, RiHome2Line } from "react-icons/ri";
+import { LuBell } from "react-icons/lu";
+import { BsPencilSquare } from "react-icons/bs";
+import { FaRegUser } from "react-icons/fa";
+import { MdOutlineLogin } from "react-icons/md";
 
 function SideMenu() {
 
     return (
         <div className='header'>
-            <div className="side-menu">
-                <button>Menú</button>
-                <button>Horarios</button>
-                <button>Notificaciones</button>
-                <button>Trámites</button>
-                <button>Perfil</button>
+            <div className="side-menu__body">
+                <a href=""> <button className='side-menu__header'> <RiHome2Line /> Menú </button> </a>
+                <a href=""> <button className=' side-menu__content'> <RiCalendarScheduleLine /> Horarios  </button> </a>
+                <a href=""> <button className=' side-menu__content'>  <LuBell /> Notificaciones</button> </a>
+                <a href=""> <button className=' side-menu__content'>  <BsPencilSquare /> Trámites </button> </a>
+                <a href=""> <button className=' side-menu__content'>  <FaRegUser /> Perfil </button> </a>
             </div>
-        <div className='side-menu__footer'>
-            <button>Cerrar sesión</button>
-        </div>
+            <div className='side-menu__footer'>
+                <a href=""> <button className='side-menu__foot'> <MdOutlineLogin /> Cerrar sesión </button> </a>
+            </div>
         </div>
     );
 }
--- frontend/src/assets/page/HomePage/Home.tsx
+++ frontend/src/assets/page/HomePage/Home.tsx
@@ -1,10 +1,12 @@
 import FrontImg from '../../componets/ComponentsHome/FrontImg/FrontIng';
 import HomeCard from '../../componets/Cards/HomeCards/HomeCard';
+import SideMenu from '#src/assets/componets/CommonComps/SideMenu/SideMenu';
 
 function Home() {
     return (
         <>
         <div className="body">
+            <SideMenu/>
             <FrontImg/>
             <HomeCard/>
             {/* Quitar luego, es solo para pruebas */}
