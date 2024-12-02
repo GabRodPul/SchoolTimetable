64ca261a8398c55912b457fe8e94e7eb1bb9ea55
DaniYezid <daniyezid@gmail.com>
2024-12-02 17:09:20 +0000

    working in sideMenu and home desktop

--- frontend/src/assets/componets/CommonComps/SideMenu/SideMenu.tsx
+++ frontend/src/assets/componets/CommonComps/SideMenu/SideMenu.tsx
@@ -1,4 +1,5 @@
-import './SideMenuStyles.css'
+import React, { useState } from 'react';
+import './SideMenuStyles.css';
 import { RiCalendarScheduleLine, RiHome2Line } from "react-icons/ri";
 import { LuBell } from "react-icons/lu";
 import { BsPencilSquare } from "react-icons/bs";
@@ -6,18 +7,62 @@ import { FaRegUser } from "react-icons/fa";
 import { MdOutlineLogin } from "react-icons/md";
 
 function SideMenu() {
+    const [active, setActive] = useState("home");
+
+    const handleSetActive = (section: React.SetStateAction<string>) => {
+        setActive(section);
+    };
 
     return (
         <div className='header'>
             <div className="side-menu__body">
-                <a href=""> <button className='side-menu__header'> <RiHome2Line /> Menú </button> </a>
-                <a href=""> <button className=' side-menu__content'> <RiCalendarScheduleLine /> Horarios  </button> </a>
-                <a href=""> <button className=' side-menu__content'>  <LuBell /> Notificaciones</button> </a>
-                <a href=""> <button className=' side-menu__content'>  <BsPencilSquare /> Trámites </button> </a>
-                <a href=""> <button className=' side-menu__content'>  <FaRegUser /> Perfil </button> </a>
+                <a href="#home">
+                    <button
+                        className={`side-menu__header ${active === "home" ? "active" : ""}`}
+                        onClick={() => handleSetActive("home")}
+                    >
+                        <RiHome2Line /> Menú
+                    </button>
+                </a>
+                <a href="#horarios">
+                    <button
+                        className={`side-menu__content ${active === "horarios" ? "active" : ""}`}
+                        onClick={() => handleSetActive("horarios")}
+                    >
+                        <RiCalendarScheduleLine /> Horarios
+                    </button>
+                </a>
+                <a href="#notificaciones">
+                    <button
+                        className={`side-menu__content ${active === "notificaciones" ? "active" : ""}`}
+                        onClick={() => handleSetActive("notificaciones")}
+                    >
+                        <LuBell /> Notificaciones
+                    </button>
+                </a>
+                <a href="#tramites">
+                    <button
+                        className={`side-menu__content ${active === "tramites" ? "active" : ""}`}
+                        onClick={() => handleSetActive("tramites")}
+                    >
+                        <BsPencilSquare /> Trámites
+                    </button>
+                </a>
+                <a href="#perfil">
+                    <button
+                        className={`side-menu__content ${active === "perfil" ? "active" : ""}`}
+                        onClick={() => handleSetActive("perfil")}
+                    >
+                        <FaRegUser /> Perfil
+                    </button>
+                </a>
             </div>
             <div className='side-menu__footer'>
-                <a href=""> <button className='side-menu__foot'> <MdOutlineLogin /> Cerrar sesión </button> </a>
+                <a href="#logout">
+                    <button className='side-menu__foot'>
+                        <MdOutlineLogin /> Cerrar sesión
+                    </button>
+                </a>
             </div>
         </div>
     );
--- frontend/src/assets/page/HomePage/Home.tsx
+++ frontend/src/assets/page/HomePage/Home.tsx
@@ -5,13 +5,18 @@ import SideMenu from '#src/assets/componets/CommonComps/SideMenu/SideMenu';
 function Home() {
     return (
         <>
-        <div className="body">
-            <SideMenu/>
-            <FrontImg/>
-            <HomeCard/>
-            {/* Quitar luego, es solo para pruebas */}
-            <button><a href='/GroupPage'> Boton Grupos provisional</a></button>
-        </div>
+            <div className="body">
+                <div className='homepage__mobile'>
+                    {/* <SideMenu/> */}
+                    <FrontImg />
+                    <HomeCard />
+                    {/* Quitar luego, es solo para pruebas */}
+                    <button><a href='/GroupPage'> Boton Grupos provisional</a></button>
+                </div>
+            </div>
+            <div className='homepage__desktop'>
+                <SideMenu />
+            </div>
         </>
     );
 }
