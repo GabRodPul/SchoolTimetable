59c06f739c35286ad45584bd425e6e1a827d406a
Smosherty <Danielmatiassmo@gmail.com>
2024-12-05 19:51:11 +0000

    mini avance en el menu derecho

---
+++ frontend/src/assets/componets/CommonComps/rigthMenu/rigthMenu.tsx
@@ -0,0 +1,23 @@
+
+
+function rigthMenu() {
+    return (
+        <div>
+            <div className="rigthMenu__title">
+                <h2>Añadir recordatorio</h2>
+            </div>
+            <div className="rigthMenu__form">
+
+            </div>
+
+            <div className="rigthMenu__title">
+                <h2>Recordatorios y Notificaciones</h2>
+            </div>
+            <div className="rigthMenu__reminders">
+                
+            </div>
+        </div>
+    );
+}
+
+export default rigthMenu;
