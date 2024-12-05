7a9f3f8f04f09d4281d27d2a5992c52543e82a4f
Smosherty <Danielmatiassmo@gmail.com>
2024-12-05 18:39:31 +0000

    otro commit antes de merge

--- frontend/src/assets/page/TimeTablePage/TimeTablePage.tsx
+++ frontend/src/assets/page/TimeTablePage/TimeTablePage.tsx
@@ -1,1 +1,15 @@
-//Hay que hacer el código
+
+
+//Desktop
+import NavigationTab from '#src/assets/componets/CommonComps/navigationTab/Teacher/NavigationTab';
+
+
+function TimeTablePage() {
+    return (
+        <div>
+            <NavigationTab/>
+        </div>
+    );
+}
+
+export default TimeTablePage;
