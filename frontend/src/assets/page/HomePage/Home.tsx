ac3b2f586479a730625782ed1ca7613d67daf901
DaniYezid <daniyezid@gmail.com>
2024-12-05 19:50:25 +0000

    fixed things

--- frontend/src/assets/page/HomePage/Home.tsx
+++ frontend/src/assets/page/HomePage/Home.tsx
@@ -1,55 +1,31 @@
-import __React, { useEffect, useState } from 'react';
-import { UserRole } from '#common/@enums/models';
-import { AuthData } from '#common/@types/models';
+import __React from 'react';
 import './HomeStyles.css'
 
 //Mobile
 import FrontImg from '../../componets/ComponentsHome/FrontImg/FrontIng';
 import Header from '#src/assets/componets/CommonComps/MenuheaderMobile/Header';
-import HomeCardAdmin from '../../componets/Cards/HomeCards/Admin/HomeCard';
-import HomeCardHeadOf from '../../componets/Cards/HomeCards/HeadOf/HomeCard';
-import HomeCardStudent from '../../componets/Cards/HomeCards/Student/HomeCard';
-import HomeCardTeacher from '../../componets/Cards/HomeCards/Teacher/HomeCard';
+import HomeCard from '../../componets/Cards/HomeCards/HomeCard';
+
 
 // Desktops
 import HomeContent from '#src/assets/componets/ComponentsHome/HomeContentDesktop/HomeContent';
 import NavigationTab from '#src/assets/componets/CommonComps/navigationTab/NavigationTab';
-import _SearchBar from '#src/assets/componets/CommonComps/SearchBarheader/SearchBarheader';
 
 function Home() {
 
-        const role = (JSON.parse(localStorage.getItem('currentUser') ?? "null") as AuthData).user.role; // Cambia 'currentuser' al nombre de la clave que usas en localStorage
-
-
     // Renderizar el componente de navegación según el rol
-    const renderHomeCard = () => {
-        switch (role) {
-            case UserRole.Admin:
-                return <HomeCardAdmin />;
-            case UserRole.Head:
-                return <HomeCardHeadOf />;
-            case UserRole.Student:
-                return <HomeCardStudent />;
-            case UserRole.Teacher:
-                return <HomeCardTeacher />;
-            default:
-                return <div>Error</div>;
-        }
-    };
-
-
     return (
         <>
             <div className="body">
                 <div className="homepage__mobile">
                     <Header />
                     <FrontImg />
-                    {renderHomeCard()}
+                    <HomeCard />
                 </div>
 
                 <div className='homepage__desktop'>
                     <div className='homepage_navigationTabs'>
-                    <NavigationTab/>
+                        <NavigationTab />
                     </div>
                     <div className="homepage__deskContent">
                         <HomeContent />
