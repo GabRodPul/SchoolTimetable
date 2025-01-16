ddc451de4e918d18ac7416ed9a1875bb85ed87fd
DaniYezid <166632468+DaniYezid@users.noreply.github.com>
2025-01-16 18:49:00 +0000

    Fixing classHour views controller

--- backend/src/controllers/views_controllers/classHour.views.controller.ts
+++ backend/src/controllers/views_controllers/classHour.views.controller.ts
@@ -5,25 +5,29 @@ import { computeError } from "../../utils/error";
 
 // Aquí debería ir una variable para guardar las cosas
 const ClassHours = DB.classHour;
+// Obtener todas las horas de clase
+const findAll = async (req: Request, res: Response) => {
+    try {
+        const data = await ClassHours.findAll();
+        res.render('ClassHour/index', { data });
+    } catch (err: any) {
+        res.send(resMsg(500, err.message));
+    }
+}
+
 
 // Métodos
-const ClassHourViewsController = Object.freeze({
-    // Obtener todas las horas de clase
-    findAll: async (req: Request, res: Response) => {
-        try {
-            const data = await ClassHours.findAll();
-            res.render('ClassHour/index', { classHours: data });
-        } catch (err: any) {
-            res.send(resMsg(500, err.message));
-        }
-    },
+export const ClassHourViewsController = Object.freeze({
+    //Guardar una hora de clase en la base de datos
+
+
 
     // Mostrar el formulario para crear una nueva hora de clase
     showCreateForm: (req: Request, res: Response) => {
         res.render('ClassHour/create');
     },
 
-    // Guardar una nueva hora de clase
+    // Crear una nueva hora de clase
     create: async (req: Request, res: Response) => {
         try {
             const newClassHour = await ClassHours.create(req.body);
@@ -50,9 +54,8 @@ const ClassHourViewsController = Object.freeze({
     // Actualizar una hora de clase
     update: async (req: Request, res: Response) => {
         const id = req.params.id;
-        const { turn, sessionHour, start, end } = req.body;
         try {
-            const [updated] = await ClassHours.update({ turn, sessionHour, start, end }, { where: { id } });
+            const [updated] = await ClassHours.update(req.body, { where: { id } });
             if (!updated) {
                 throw new Error(`ClassHour with id ${id} not found`);
             }
@@ -76,5 +79,3 @@ const ClassHourViewsController = Object.freeze({
         }
     },
 });
-
-export { ClassHourViewsController };
